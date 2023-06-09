ac1fd43f1d2e3c7f7145a9645c68105c5eb6ebf9
vipul <kumarvipul685@gmail.com>
2023-06-09 14:02:33 +0530

    added pointers in playerHandler

--- lib/services/audio_player_handler.dart
+++ lib/services/audio_player_handler.dart
@@ -1,15 +1,29 @@
+import 'package:flutter/foundation.dart';
 import 'package:just_audio/just_audio.dart';
 
 /// it plays audio given audio source
 
 class AudioPlayerHandler {
   static AudioPlayerHandler? _instance;
-  AudioPlayerHandler._();
+  AudioPlayerHandler._() {
+    _init();
+  }
 
   factory AudioPlayerHandler() {
     _instance ??= AudioPlayerHandler._();
     return _instance!;
   }
 
-  final audioPlayer = AudioPlayer();
+  final _audioPlayer = AudioPlayer();
+  Stream<Duration> get positionStream => _audioPlayer.positionStream;
+  Stream<Duration?> get durationStream => _audioPlayer.durationStream;
+
+  void dispose() {
+    _audioPlayer.dispose();
+  }
+
+  void _init() {}
+  void pause() {}
+  void play() {}
+  void updatePosition(double pos) {}
 }
